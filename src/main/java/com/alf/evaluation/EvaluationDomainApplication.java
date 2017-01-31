6b6b95e992bdc823a8a03147f1d13439e420a951
kaleabt <kaleabt1@gmail.com>
2017-01-30 21:46:20 -0600

    add spring boot starter project skeleton

---
+++ src/main/java/com/alf/evaluation/EvaluationDomainApplication.java
@@ -0,0 +1,12 @@
+package com.alf.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationDomainApplication.class, args);
+	}
+}
